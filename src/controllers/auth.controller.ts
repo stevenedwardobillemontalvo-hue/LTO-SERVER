7fc1591ae318ef2fd282b02f0facf1f068fa4658
stevenedwardobillemontalvo-hue <stevenedwardobillemontalvo@gmail.com>
2026-02-25 05:46:18 +0800

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -82,6 +82,14 @@ export const register = async (req: Request, res: Response) => {
       userData.isVerified = true; 
       userData.verificationToken = null; 
     }
+    const user = await User.create(userData);
+
+    if (type === "client") {
+      const verificationToken = randomBytes(32).toString("hex");
+      userData.verificationToken = verificationToken;
+      const user = await User.create(userData);
+      await sendVerificationEmail(user.email, user.firstName, verificationToken);
+    }
 
     res.status(201).json({
       success: true,
